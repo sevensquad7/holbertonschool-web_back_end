1b659e8ceb3a7cd0f71b89ca80c68d0e55a5b140
sevensquad7 <edgarxot@gmail.com>
2022-09-19 13:02:11 -0500

    correct pycodestyle

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,11 +5,11 @@ sum_list function
 from typing import List
 
 
-def sum_list(input_list : List[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
    return sum of a list floats
     """
     listsum = 0
     for item in input_list:
-        listsum+=item;
+        listsum += item
     return listsum
