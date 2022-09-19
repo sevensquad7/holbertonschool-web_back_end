83995218037e5714a5e7a233fadb53669735edc0
sevensquad7 <edgarxot@gmail.com>
2022-09-19 13:48:08 -0500

    Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+zoom_array function
+"""
+from typing import Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """
+    Use mypy to validate
+    """
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
