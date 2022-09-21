b62d9f272f588e58ab41530eb3a5d23f9a01dadb
sevensquad7 <edgarxot@gmail.com>
2022-09-21 00:08:22 -0500

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,12 +6,12 @@ import asyncio
 from typing import List
 
 
-task_wait_random = __import__('3-tasks').task_wait_random
+task_wait_random = __import__('1-concurrent_coroutines').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    return the list 
+    return the list
     """
-    ord = await asyncio.gather(*[task_wait_random(max_delay) for y in range(n)])
-    return sorted(ord)
+    od = await asyncio.gather(*[task_wait_random(max_delay) for y in range(n)])
+    return sorted(od)
