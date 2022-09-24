6026247c36446ff5bbb41a7ae1241d7d8a2cc0a3
sevensquad7 <edgarxot@gmail.com>
2022-09-23 23:50:18 -0500

    Basic dictionary

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    This caching system doesn’t have limit
+    """
+    def put(self, key, item):
+        """
+        if doesn’t exist, not do anything
+        """
+        if not key or not item:
+            pass
+        else:
+            self.cache_data[key]=item
+
+    def get(self, key):
+        """
+        if doesn’t exist, return none
+        """
+        if key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
