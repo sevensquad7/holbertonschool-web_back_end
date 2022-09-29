6d1b720465cf7eaed13d2587b665bc63fb9cc370
sevensquad7 <edgarxot@gmail.com>
2022-09-28 22:26:00 -0500

    FIFO caching

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -6,10 +6,19 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """
+    FIFOCache class
+    """
     def __init__(self):
+        """
+        don’t forget to call the parent init: super().__init__()
+        """
         super().__init__()
 
     def put(self, key, item):
+        """
+        if doesn’t exist, not do anything
+        """
         MAX_ITEMS = len(self.cache_data.items())
         if not key or not item:
             pass
@@ -21,6 +30,9 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        if doesn’t exist, return none
+        """
         if not key or key not in self.cache_data.keys():
             return None
         else:
