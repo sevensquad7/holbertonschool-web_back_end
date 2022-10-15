353fc7eac0b0ab71b5fc9be16cb666548bc7e7fe
sevensquad7 <edgarxot@gmail.com>
2022-10-15 16:10:11 -0500

    LIFO Caching

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+LIFOCache is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache class
+    """
+    def __init__(self):
+        """
+        don’t forget to call the parent init: super().__init__()
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        if doesn’t exist, not do anything
+        """
+        if key is None or item is None:
+            return
+        if (
+            len(self.cache_data.items()) == BaseCaching.MAX_ITEMS
+            and (key not in self.cache_data.keys())
+        ):
+            lastItem = list(self.cache_data)[len(self.cache_data.items())-1]
+            print("DISCARD:", lastItem)
+            self.cache_data.pop(lastItem)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        if doesn’t exist, return none
+        """
+        if not key or key not in self.cache_data.key():
+            return None
+        else:
+            return self.cache_data[key]
