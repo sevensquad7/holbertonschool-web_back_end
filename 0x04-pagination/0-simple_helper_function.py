45864e1c84505e43e6e786f8191ee1e5c1dd22e3
sevensquad7 <edgarxot@gmail.com>
2022-10-15 20:45:45 -0500

    Simple helper function

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+index_range is a pagination function
+"""
+
+
+def index_range(page: int, page_size:int) -> tuple:
+    """
+    Page numbers are 1-indexed, i.e. the first page is page 1.
+    """
+    return (((page - 1) * page_size), (page * page_size))
