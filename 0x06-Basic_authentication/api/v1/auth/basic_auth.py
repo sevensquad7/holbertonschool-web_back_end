391f4c07acc37acd0dc8a5add560701e4374b3b8
sevensquad7 <edgarxot@gmail.com>
2022-10-24 16:20:00 -0500

    Basic - User object

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic API authentication module
 """
 
 from base64 import b64decode
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 
 
@@ -51,3 +52,24 @@ class BasicAuth(Auth):
             res = decoded_base64_authorization_header.split(":", 1)
             return (res[0], res[1])
         return (None, None)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        self descriptive
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
