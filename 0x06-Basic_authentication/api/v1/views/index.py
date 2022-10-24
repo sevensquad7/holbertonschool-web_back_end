ede6217b07b6e6b952a3bbdf7e30739e447c0124
sevensquad7 <edgarxot@gmail.com>
2022-10-24 13:01:35 -0500

    Error handler: Unauthorized

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return abort(401)
