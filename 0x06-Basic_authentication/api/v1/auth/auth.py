2033e644c27cc99f74158dfc81c362a1c7cc8f4f
sevensquad7 <edgarxot@gmail.com>
2022-10-24 13:24:55 -0500

    Auth class

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -6,11 +6,19 @@ from flask import request
 
 
 class Auth():
+    """ Class Authentication
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ method require_auth
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ method authorization_header
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ method current_user
+        """
         return None
