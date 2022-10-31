eb5ac70dc0bbdb6e0a8d8a87c38622724adfddc4
sevensquad7 <edgarxot@gmail.com>
2022-10-31 11:50:23 -0500

    Basic Flask app

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Welcome Holberton
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    language config
+    """
+    LANGUAGES = ['en', 'es']
+
+@app.route("/", methods=['GET'])
+def helloWorld():
+    """
+    Hello world
+    """
+    return render_template('0-index.html')
