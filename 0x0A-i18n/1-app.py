e540293cd19897290978197783e4fca8cc4c7aa7
sevensquad7 <edgarxot@gmail.com>
2022-10-31 11:50:55 -0500

    Basic Babel setup

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Welcome Holberton
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    language config
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=['GET'])
+def helloWorld():
+    """
+    Hello world
+    """
+    return render_template('1-index.html')
