147b52f15805fbd995b39e449e7ecc156dfd67a0
sevensquad7 <edgarxot@gmail.com>
2022-10-31 19:50:11 -0500

    Use user locale

--- 0x0A-i18n/6-app.py
+++ 0x0A-i18n/6-app.py
@@ -64,7 +64,7 @@ def before_request():
 @app.route("/", methods=["GET"])
 def index():
     """ Returns index """
-    return render_template("5-index.html")
+    return render_template("6-index.html")
 
 
 if __name__ == "__main__":
