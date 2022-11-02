de973cc7065244816913c8d28ee2efb59740d8f0
sevensquad7 <edgarxot@gmail.com>
2022-11-01 23:37:30 -0500

    Retrieving lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,10 +2,57 @@
 """
 Redis
 """
+from curses import keyname
 import sys
 from typing import Callable, Optional, Union
 import uuid
 import redis
+from functools import wraps
+
+
+def replay(fn):
+    """ display the history of calls of a particular function """
+    store = redis.Redis()
+    count_key = fn.__qualname__
+    input_key = count_key + ":inputs"
+    output_key = count_key + ":outputs"
+    count = store.get(count_key).decode("utf-8")
+    print("{} was called {} times:".format(count_key, count))
+    inputs = store.lrange(input_key, 0, count)
+    outputs = store.lrange(output_key, 0, count)
+    for input, output in zip(inputs, outputs):
+        input = input.decode("utf-8")
+        output = output.decode("utf-8")
+        print("{}(*{}) -> {}".format(count_key, input, output))
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Call counter decorator
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Method wrapper to incr count """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """ Calls call history decorator """
+    key = method.__qualname__
+    input_key = key + ":inputs"
+    output_key = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Method wrapper to push history in store """
+        self._redis.rpush(input_key, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(data))
+        return data
+    return wrapper
 
 
 class Cache():
@@ -16,6 +63,8 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store Method
