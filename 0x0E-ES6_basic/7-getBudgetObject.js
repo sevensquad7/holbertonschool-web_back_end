72e6cd5a83e7e80981c3084c121e292ba898bdfd
sevensquad7 <edgarxot@gmail.com>
2023-01-30 09:42:36 -0500

    load file task_7

--- 0x0E-ES6_basic/7-getBudgetObject.js
+++ 0x0E-ES6_basic/7-getBudgetObject.js
@@ -1,2 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
-    return budget = { income, gdp, capita };}
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+  return budget;
+}
